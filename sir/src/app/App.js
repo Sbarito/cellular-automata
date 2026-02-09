a60330345666649a7273f674dca36f3bb024722b
sofya7779 <sofya7779@yandex.ru>
2026-02-10 01:32:25 +0300

    init: initialization first stage project

---
+++ sir/src/app/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className='header'>
+        <div>SIR model</div>
+        <div style={{display: 'flex', gap: '20px'}}>
+          <div>Имулятор</div>
+          <div>Инструкция</div>
+          <div>О проекте</div>
+        </div>
+      </header>
+      <footer className='footer'>контакты для связи:<br/>почта: 2b1000okn35@gmail.com<br/>телефон: +79872767937</footer>
+    </div>
+  );
+}
+
+export default App;
