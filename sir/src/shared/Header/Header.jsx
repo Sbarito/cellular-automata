63f3fc3377e3cfc0e239b5d4555187c8756220b8
sofya7779 <sofya7779@yandex.ru>
2026-03-10 00:01:44 +0300

    fix: remove unnessesary sides in navbar

--- sir/src/shared/Header/Header.jsx
+++ sir/src/shared/Header/Header.jsx
@@ -2,20 +2,6 @@ const Header = () => {
     return (
        <header className='header'>
         <div>SIR model</div>
-        <div style={{display: 'flex', gap: '20px'}}>
-          <a 
-            href="" 
-            style={{textDecoration: 'none', color: 'inherit'}}
-          >
-            Симулятор
-          </a>
-          <a 
-            href="" 
-            style={{textDecoration: 'none', color: 'inherit'}}
-          >
-            Справка
-          </a>
-        </div>
       </header>
     )
 }
